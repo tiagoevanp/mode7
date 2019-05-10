7fd0803d866fcc1c51bd4cf125f2e541c32d00c2
Tiago Evan <tiagoevanp@hotmail.com>
2019-05-10 00:08:04 -0300

    FUCK YEAH

--- index.js
+++ index.js
@@ -4,22 +4,41 @@ window.onload = function () {
 
 	var cam = {
 		x: 400,
-		y: 500,
-		z: 40
+		y: 400,
+		z: 100
 	}
 	
 	var square = {
-		x: cam.x-100,
-		y: cam.y-150,
-		z: cam.z-50,
-		width: 200,
-		height: 200
+		xA: cam.x-100,
+		xB: cam.x+100,
+		xC: cam.x+100,
+		xD: cam.x-100,
+		yA: cam.y-150,
+		yB: cam.y-150,
+		yC: cam.y+10,
+		yD: cam.y+10,
+		zA: cam.z-50,
+		zB: cam.z-50,
+		zC: cam.z-100,
+		zD: cam.z-100,
 	}
 	
 	var points = calculatePointsPositionInZeroZ();
 	console.log(points);
+	drawPathIntoPoints(points);
 	drawPoints(points);
 	
+	function drawPathIntoPoints(points) {
+		ctx.strokeStyle = 'white';
+		ctx.beginPath();
+		ctx.moveTo(points.a.x, points.a.y);
+    ctx.lineTo(points.b.x, points.b.y);
+    ctx.lineTo(points.c.x, points.c.y);
+		ctx.lineTo(points.d.x, points.d.y);
+		ctx.closePath();
+		ctx.stroke();
+	}
+
 	function drawPoints(points) {
 		ctx.fillStyle = 'green';
 		createPoint(points.a.x, points.a.y);
@@ -27,36 +46,41 @@ window.onload = function () {
 		createPoint(points.c.x, points.c.y);
 		createPoint(points.d.x, points.d.y);
 		ctx.fillStyle = 'red';
-		createPoint(square.x, square.y);
-		createPoint(square.x+square.width, square.y);
-		createPoint(square.x+square.width, square.y+square.height);
-		createPoint(square.x, square.y+square.height);
+		createPoint(square.xA, square.yA);
+		createPoint(square.xB, square.yB);
+		createPoint(square.xC, square.yC);
+		createPoint(square.xD, square.yD);
 		ctx.fillStyle = 'blue';
 		createPoint(cam.x, cam.y);
 	}
 
 	function calculatePointsPositionInZeroZ() {
-		var lambda = -cam.z/(square.z-cam.z);
+		var lambda = cam.z/(cam.z-square.zA);
 		var pointA = {
-			x: square.x+(square.x-cam.x)*lambda,
-			y: square.y+(square.y-cam.y)*lambda,
+			x: lambda*(square.xA-cam.x)+cam.x,
+			y: lambda*(square.yA-cam.y)+cam.y,
 			z: 0
 		}
+		
+		lambda = -cam.z/(square.zB-cam.z);
 		var pointB = {
-			x: square.x+square.width+(square.x+square.width-cam.x)*lambda,
-			y: square.y+(square.y-cam.y)*lambda,
+			x: lambda*(square.xB-cam.x)+cam.x,
+			y: lambda*(square.yB-cam.y)+cam.y,
 			z: 0
 		}
+		
+		lambda = -cam.z/(square.zC-cam.z);
 		var pointC = {
-			x: square.x+square.width+(square.x+square.width-cam.x)*lambda,
-			y: square.y+square.height+(square.y+square.height-cam.y)*lambda,
+			x: lambda*(square.xC-cam.x)+cam.x,
+			y: lambda*(square.yC-cam.y)+cam.y,
 			z: 0
 		}
+		
+		lambda = -cam.z/(square.zD-cam.z);
 		var pointD = {
-			x: square.x+(square.x-cam.x)*lambda,
-			y: square.y+square.height+(square.y+square.height-cam.y)*lambda,
+			x: lambda*(square.xD-cam.x)+cam.x,
+			y: lambda*(square.yD-cam.y)+cam.y,
 			z: 0
-
 		}
 		return {
 			a: pointA, 
