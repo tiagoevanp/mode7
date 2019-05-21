febdb4f9e9ce4633e2cee64234e30fcd1628e0a9
Tiago Evan <tiagoevanp@hotmail.com>
2019-05-21 00:40:29 -0300

    CONSEGUI

--- fs.glsl
+++ fs.glsl
@@ -4,12 +4,16 @@ function fs() {
 			precision highp float;
 		#endif
 
+		#define USE_MAP true
+
 		uniform vec2 u_resolution;
 		uniform float u_time;
+		uniform sampler2D u_map;
+		uniform vec2 u_points[4];
 
 		void main() {
 			vec2 st = gl_FragCoord.xy/u_resolution;
-			gl_FragColor=vec4(st.y-st.x, st.x-st.y, 0.0, 1.0);
+			gl_FragColor= texture2D(u_map, vec2(u_points[0].x+st.x/8.0, 0.8+st.y/8.0));
 		}
 	
 	`;
--- gl.js
+++ gl.js
@@ -1,6 +1,10 @@
 var container;
 var camera, scene, renderer;
 var uniforms;
+var texture;
+var points;
+var matrix;
+var size = 0.01;
 
 init();
 animate();
@@ -11,14 +15,26 @@ function init() {
 		camera = new THREE.Camera();
 		camera.position.z = 1;
 
+		texture = new THREE.TextureLoader().load( "img2.png" );
+
 		scene = new THREE.Scene();
 
+
 		var geometry = new THREE.PlaneBufferGeometry( 2, 2 );
+		
+		matrix = [
+			new THREE.Vector2(),
+			new THREE.Vector2(),
+			new THREE.Vector2(),
+			new THREE.Vector2(),
+		];
 
 		uniforms = {
-				u_time: { type: "f", value: 1.0 },
+				u_time: { type: "f", value: 0.0 },
 				u_resolution: { type: "v2", value: new THREE.Vector2() },
-				u_mouse: { type: "v2", value: new THREE.Vector2() }
+				u_mouse: { type: "v2", value: new THREE.Vector2() },
+				u_points: { type: "v2v",  value: matrix },
+				u_map: { type: "t", value: texture }
 		};
 
 		var material = new THREE.ShaderMaterial( {
@@ -26,6 +42,8 @@ function init() {
 				vertexShader: vs(),
 				fragmentShader: fs()
 		} );
+		
+		// var material = new THREE.MeshBasicMaterial( { map: texture} );
 
 		var mesh = new THREE.Mesh( geometry, material );
 		scene.add( mesh );
@@ -49,9 +67,21 @@ function init() {
 function animate() {
 		requestAnimationFrame( animate );
 		render();
-}
+	}
+	
+	function render() {
+		points = calculatePointsPositionInZeroZ();
+		
+		uniforms.u_points.value[0].x = points.a.x/1000;
+		uniforms.u_points.value[0].y = points.a.y/1000;
+		uniforms.u_points.value[1].x = points.b.x/1000;
+		uniforms.u_points.value[1].y = points.b.y/1000;
+		uniforms.u_points.value[2].x = points.c.x/1000;
+		uniforms.u_points.value[2].y = points.c.y/1000;
+		uniforms.u_points.value[3].x = points.d.x/1000;
+		uniforms.u_points.value[3].y = points.d.y/1000;
 
-function render() {
 		uniforms.u_time.value += 0.05;
+		
 		renderer.render( scene, camera );
 }
